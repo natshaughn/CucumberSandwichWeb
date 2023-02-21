72045b7691d13ba8b771cdd0a7056fc874479761
Nathan Shaughnessy <nathan.shaughnessy@roq.local>
2023-02-21 15:57:50 +0000

    3 scenarios and 7 steps passed

--- features/index.feature
+++ features/index.feature
@@ -2,13 +2,13 @@ Feature: Testing website
 
     Scenario: I should see the correct title
         Given the webpage has loaded
-        Then the heading text should read Random website
+        Then the heading text should read "Cucumber Sandwich"
 
     Scenario: The button should change color when clicked
-        When the button is clicked
+        When button clicked
         Then the color should be darkred
 
     Scenario: The button is clicked twice 
-        When the button is clicked
-        And the button is clicked again
+        When button clicked
+        And button clicked
         Then the color should be cornflowerblue
--- features/support/steps.js
+++ features/support/steps.js
@@ -1,10 +1,45 @@
 const assert = require('assert');
-const {Given, When, Then} = require('@cucumber/cucumber');
+const {Given, When, Then, Before, After} = require('@cucumber/cucumber');
+const Browser = require("../../Browser");
+const browser = new Browser();
+const { By } = require('selenium-webdriver');
 
-Given("the webpage has loaded", function() {
-    assert(true);
+Before(async function() {
+    browser.browserBuild();
+    await browser.browserNavigate('http://127.0.0.1:5500/');
 });
 
-Then("the heading text should read Random website", function() {
-    assert(true);
+After(async function() {
+    await browser.browserExit();
+});
+
+Given("the webpage has loaded", function() {
+    // Nothing to do as page already loaded
+});
+
+
+When("button clicked", async function() {
+    await browser.elementClick('navbarButton')
+});
+
+    
+// When("the button is clicked again", function() {
+//     assert(true);
+// });
+
+Then("the heading text should read {string}", async function(title) {
+    const actual = await browser.headless.getTitle();
+    // expect(actual).toBe('Cucumber Sandwich');
+    assert(actual == title);
+});
+
+Then("the color should be darkred", async function() {
+    const actual = await browser.elementFillColorCss("navbarButton");
+    assert(actual == 'rgba(139, 0, 0, 1)');
+});
+
+
+Then("the color should be cornflowerblue", async function() {
+    const actual = await browser.elementFillColorCss("navbarButton");
+    assert(actual == 'rgba(100, 149, 237, 1)');
 });
